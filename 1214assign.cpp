b62ef4f77821baf93d68f73a3ddfad13850f22bc
manifold <mayesheng@sjtu.edu.cn>
2015-10-29 12:46:56 +0800

    1213 & 1214

--- 1214assign.cpp
+++ 1214assign.cpp
@@ -1,122 +1,127 @@
 #include <iostream>
-#include <queue>
 using namespace std;
 
-struct node
-{
-	node *left;
-	node *parent;
-	node *right;
-	int value;
-
-	node()
-	{
-		left = right = parent = 0;
-		value = -1;
-	}
+struct node {
+	node *s = 0;
+	node *f = 0;
+	node *yb = 0;
+	node *ob = 0;
+	int val = 0;
 };
 
-int N, M;
+node *tree[100000 + 5];
+node *root;
+int N, p, q, v;
 
-int al[100000 + 10];
-int ar[100000 + 10];
-int av[100000 + 10];
-int bl[100000 + 10];
-int br[100000 + 10];
-int bv[100000 + 10];
+node **queue = tree;
+int head = 0, tail = -1;
 
-node *ta[100000 + 10];
-node *tb[100000 + 10];
+void findroot();
+void preTra(node *p);
+void postTra(node *p);
+void levTra();
 
-node *rta;
-node *rtb;
-
-node *findroot(node *p)
-{
-	while (p->parent != 0)
-		p = p->parent;
-	return p;
-}
 
 int main()
 {
 	cin >> N;
+	for (int i = 1; i <= N; ++i)
+		tree[i] = new node;
 	for (int i = 1; i <= N; ++i) {
-		cin >> al[i] >> ar[i] >> av[i];
-		ta[i] = new node;
-	}
-	cin >> M;
-	for (int i = 1; i <= M; ++i) {
-		cin >> bl[i] >> br[i] >> bv[i];
-		tb[i] = new node;
-	}
-	//special case
-	if (N != M) {
-		cout << 'N' << endl;
-		return 0;
-	}
-
-	for (int i = 1; i <= N; ++i) {
-		if (al[i] != 0)	{
-			ta[i]->left = ta[al[i]];
-			ta[al[i]]->parent = ta[i];
+		cin >> p >> q >> v;
+		if (p != 0) {
+			tree[i]->s = tree[p];
+			tree[p]->f = tree[i];
 		}
-		if (ar[i] != 0) {
-			ta[i]->right = ta[ar[i]];
-			ta[ar[i]]->parent = ta[i];
+		if (q != 0) {
+			tree[i]->yb = tree[q];
+			tree[q]->ob = tree[i];
 		}
-		ta[i]->value = av[i];
+		tree[i]->val = v;
 	}
-	//link, non-empty node's parent and l, r are linked
-	for (int i = 1; i <= N; ++i) {
-		if (bl[i] != 0)	{
-			tb[i]->left = tb[bl[i]];
-			tb[bl[i]]->parent = tb[i];
-		}
-		if (br[i] != 0) {
-			tb[i]->right = tb[br[i]];
-			tb[br[i]]->parent = tb[i];
-		}
-		tb[i]->value = bv[i];
-	}
-
-	rta = findroot(ta[1]);
-	rtb = findroot(tb[1]);
-	queue<node *> lva;
-	queue<node *> lvb;
-	lva.push(rta);
-	lvb.push(rtb);
-	int cnt = 0; //nodes that has been processed
-
-	while (cnt < N) {
-		if (lva.front()->value != lvb.front()->value) {
-			cout << 'N' << endl;
-			return 0;
-		}
-		if (lva.front()->value != -1)
-			++cnt;
-		//push and pop a
-		if (lva.front()->left != 0)
-			lva.push(lva.front()->left);
-		else
-			lva.front()->left = new node;
-		if (lva.front()->right != 0)
-			lva.push(lva.front()->right);
-		else
-			lva.front()->right = new node;
-		lva.pop();
-		//push and pop a
-		if (lvb.front()->left != 0)
-			lvb.push(lvb.front()->left);
-		else
-			lvb.front()->left = new node;
-		if (lvb.front()->right != 0)
-			lvb.push(lvb.front()->right);
-		else
-			lvb.front()->right = new node;
-		lvb.pop();
-	}
-	cout << 'Y' << endl;
+	root = tree[1];
+	findroot();
+	preTra(root); cout << endl;
+	postTra(root); cout << endl;
+	levTra(); cout << endl;
 	//while (1);
 	return 0;
+}
+
+void findroot() {
+	while (root->f != 0 || root->ob != 0 || root->yb != 0) {
+		if (root->f != 0)
+			root = root->f;
+		else if (root->ob != 0)
+			root = root->ob;
 	}
+}
+
+void preTra(node *p) {
+	cout << p->val << ' ';
+	node *tmp0 = p;
+	while (tmp0->yb != 0 && p == root)  {
+		preTra(tmp0->yb);
+		tmp0 = tmp0->yb;
+	}
+	if (p->s != 0) {
+		preTra(p->s);
+		node *tmp = p->s;
+		while (tmp->yb != 0)  {
+			preTra(tmp->yb);
+			tmp = tmp->yb;
+		}
+	}
+}
+
+void postTra(node *p) {
+	node *tmp0 = p;
+	while (tmp0->yb != 0 && p == root)  {
+		preTra(tmp0->yb);
+		tmp0 = tmp0->yb;
+	}
+	if (p->s != 0) {
+		postTra(p->s);
+		node *tmp = p->s;
+		while (tmp->yb != 0)  {
+			postTra(tmp->yb);
+			tmp = tmp->yb;
+		}
+	}
+	cout << p->val << ' ';
+}
+
+void levTra() {
+	queue[++tail] = root;
+	node *tmp0 = root;
+	while (tmp0->yb != 0) {
+		tmp0 = tmp0->yb;
+		queue[tail++] = tmp0;
+	}
+	while (tail - head + 1 != 0) {
+		node *tmp = queue[head];
+		cout << tmp->val << ' ';
+		if (tmp->s != 0) {
+			tmp = tmp->s;
+			queue[++tail] = tmp;
+			while (tmp->yb != 0) {
+				tmp = tmp->yb;
+				queue[++tail] = tmp;
+			}
+		}
+		++head;
+	}
+}
+
+/*
+9
+2 0 1
+3 4 2
+0 0 3
+5 6 4
+0 8 5
+0 7 6
+0 0 7
+9 0 8
+0 0 9
+*/
